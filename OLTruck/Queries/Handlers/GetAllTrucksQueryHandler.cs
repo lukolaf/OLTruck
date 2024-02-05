6bdfd3ec45e097bd4dd3706c8b4d5cc5b9ef5055
zoniferodev <devmail@zonifero.com>
2024-02-05 13:02:20 +0100

    Added sorting and filtering for getAllTrucks

--- OLTruck/Queries/Handlers/GetAllTrucksQueryHandler.cs
+++ OLTruck/Queries/Handlers/GetAllTrucksQueryHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Mapster;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using OLTruck.Domain.Models;
 using OLTruck.Infrastructure;
 using OLTruck.Shared.TruckDto;
 
@@ -17,8 +18,32 @@ namespace OLTruck.Queries.Handlers
 
         public async Task<IEnumerable<TruckDto>> Handle(GetAllTrucksQuery request, CancellationToken cancellationToken)
         {
-            var result = await _context.Trucks.ToListAsync();
-            return result.Adapt<List<TruckDto>>();
+            var query = _context.Trucks.AsQueryable();
+            query = ApplyFilter(query, request.Filter);
+            query = ApplySorting(query, request.SortBy);
+
+            return await query.Select(t => t.Adapt<TruckDto>()).ToListAsync(cancellationToken);
+        }
+        private IQueryable<Truck> ApplyFilter(IQueryable<Truck> query, string? filter)
+        {
+            if (!string.IsNullOrEmpty(filter))
+            {
+                query = query.Where(t => t.Name.Contains(filter) ||
+                                         t.Description.Contains(filter) ||
+                                         t.Code.Contains(filter));
+            }
+            return query;
+        }
+        private IQueryable<Truck> ApplySorting(IQueryable<Truck> query, string? sortBy)
+        {
+            return sortBy switch
+            {
+                "name" => query.OrderBy(t => t.Name),
+                "description" => query.OrderBy(t => t.Description),
+                "code" => query.OrderBy(t => t.Code),
+                "status" => query.OrderBy(t => t.Status),
+                _ => query
+            };
         }
     }
 }
